0d6604a5b44fdc9607cfabbf0f1340e431ea81e1
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-17 02:58:22 +0600

    google authentication implement

--- src/Components/SocialMedia/SocialMedia.js
+++ src/Components/SocialMedia/SocialMedia.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import auth from '../../firebase.init';
 import google from '../../images/google.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 const SocialMedia = () => {
 
-    const [signInWithMicrosoft, user, loading, error] = useSignInWithMicrosoft(
-        auth
-    );
-
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user)
+    }
     return (
-        <div className="w-50 mx-auto py-1 rounded-pill d-flex align-items-center btn-primary mt-5 btn justify-content-center">
+        <div onClick={() => signInWithGoogle()} className="w-50 mx-auto py-1 rounded-pill d-flex align-items-center btn-primary mt-5 btn justify-content-center">
+            {
+                loading && <div class="spinner-grow" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            }
             <img src={google} height='30' width='30' className="rounded-circle" alt="" />
             <div className="align-items-self-center text-white ms-3">Sign With Google</div>
         </div>
